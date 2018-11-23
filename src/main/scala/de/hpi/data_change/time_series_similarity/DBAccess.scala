dcc2de5f42937df6661484986a4ee272f097ed74
leon <leon.bornemann@hpi.de>
2018-11-23 12:56:32 +0100

    Simplified Database Access

--- src/main/scala/de/hpi/data_change/time_series_similarity/DBAccess.scala
+++ src/main/scala/de/hpi/data_change/time_series_similarity/DBAccess.scala
@@ -10,94 +10,46 @@ case class DBAccess(spark:SparkSession,config:Config) extends Serializable {
 
     import spark.implicits._
 
-    def writeToDB(resultDF: DataFrame,centers:Seq[Array[Double]],inputSchema:Seq[String]) = {
-        val centerDF = spark.createDataset(centers.zipWithIndex)
-
-        val df1 = resultDF.toDF()
-            .withColumn("rowNr",monotonically_increasing_id())
-            .select("name","assignedCluster","rowNr")
-        df1.show()
-        val splitAndExploded = df1.select(
-            $"rowNr",
-            $"assignedCluster",
-            posexplode($"name")
-        )
-        splitAndExploded.show()
-        val dfNew = splitAndExploded.groupBy("rowNr","assignedCluster").pivot("pos").agg(first("col"))
-        dfNew.show()
-        val finalDF = dfNew.drop("rowNr")
-
-        val oldOrder = finalDF.columns
-        val newOrder = oldOrder.slice(1, oldOrder.size) ++ Seq(oldOrder(0))
-        val finalDFReordered = finalDF.select(newOrder.head, newOrder.tail: _*)
-
-        val oldCols = finalDFReordered.columns.slice(0, finalDFReordered.columns.size - 1)
-        val originalNames = inputSchema.slice(0, inputSchema.size - 1)
-        var finalColumnsRenamed = finalDFReordered
-
-        oldCols.zip(originalNames).foreach { case (old, new_) => {
-            finalColumnsRenamed = finalColumnsRenamed.withColumnRenamed(old, new_)
-        }
-        }
-        finalColumnsRenamed.show()
-
-
-        val toWrite = resultDF.map(r => {
-            val keyArray = r.getAs[Seq[String]](0)
-            val cluster = r.getAs[Int]("assignedCluster")
-            (keyArray.mkString(Clustering.KeySeparator),cluster)
-        }).toDF()
-            .withColumn("rowNr",monotonically_increasing_id())
-
-//        val finalFinalDF = getResultDF(toWrite,inputSchema)
-
-
-        println("dummy output simulating database write")
+    def writeDFToDB(df: DataFrame,tableName:String) = {
         val props = new Properties()
         props.setProperty("user",config.user)
         props.setProperty("password",config.password)
         props.setProperty("driver",config.driver)
-        finalColumnsRenamed.write.jdbc(config.url,config.configIdentifier,props)
-        centerDF.write.jdbc(config.url,config.configIdentifier + "_centers",props)
-        toWrite.write.jdbc(config.url,config.configIdentifier + "_old",props)
+        df.write.jdbc(config.url,tableName,props)
     }
 
-    private def getResultDF(toWrite:DataFrame,inputSchema:Seq[String]) = {
-        //split by key separator and
-        val toWriteColumns = toWrite.columns
-        val clusterCol = $"${toWriteColumns(1)}"
-        val columnToSplit = $"${toWriteColumns(0)}"
-        val splitAndExploded = toWrite.select(
-            clusterCol,
+    def writeToDB(resultDF: DataFrame, centers:Seq[Array[Double]], inputSchema:Seq[String]) = {
+        //cluster centers
+        val centerDF = spark.createDataset(centers.zipWithIndex)
+        //drop the time series values:
+        val df1 = resultDF.toDF()
+            .withColumn("rowNr",monotonically_increasing_id())
+            .select("name","assignedCluster","rowNr")
+        //explode key array:
+        val keyArrayExploded = df1.select(
             $"rowNr",
-            split(columnToSplit, Clustering.KeySeparator).alias("parts"),
-            posexplode(split(columnToSplit, Clustering.KeySeparator))
+            $"assignedCluster",
+            posexplode($"name")
         )
-        val concatenated = splitAndExploded
-            .drop($"col").select(
-            $"rowNr",
-            concat(lit("letter"), col("pos").cast("string")).alias("name"),
-            expr("parts[pos]").alias("val"),
-            clusterCol.alias("Cluster")
-        )
-        val clusterColName = concatenated.columns(concatenated.columns.length - 1)
-        //groupBy should be group by row number!
-        val pivoted = concatenated.groupBy("rowNr", clusterColName).pivot("name").agg(first("val"))
-        val oldOrder = pivoted.columns
-        val newOrder = Seq(oldOrder(0)) ++ oldOrder.slice(2, oldOrder.size) ++ Seq(oldOrder(1))
-        val columnsReordered = pivoted.select(newOrder.head, newOrder.tail: _*)
-            .drop("rowNr")
-        columnsReordered.show()
-        val oldCols = columnsReordered.columns.slice(0, columnsReordered.columns.size - 1)
+        //use grouping and pivoting to create one column per element in the original array:
+        var finalDF = keyArrayExploded.groupBy("rowNr","assignedCluster").pivot("pos").agg(first("col"))
+            .drop("rowNr") //no longer needed
+        //reorder the dataframe so that assignedCluster is the last column:
+        val oldOrder = finalDF.columns
+        val newOrder = oldOrder.slice(1, oldOrder.size) ++ Seq(oldOrder(0))
+        finalDF = finalDF.select(newOrder.head, newOrder.tail: _*)
+        //rename the first n-1 columns to the original input columns:
+        val currentNames = finalDF.columns.slice(0, finalDF.columns.size - 1)
         val originalNames = inputSchema.slice(0, inputSchema.size - 1)
-        assert(oldCols.length == originalNames.size)
-        var finalColumnsRenamed = columnsReordered
-        oldCols.zip(originalNames).foreach { case (old, new_) => {
-            finalColumnsRenamed = finalColumnsRenamed.withColumnRenamed(old, new_)
-            }
+        currentNames.zip(originalNames).foreach { case (old, new_) => {
+            finalDF = finalDF.withColumnRenamed(old, new_)
+          }
         }
-        finalColumnsRenamed = finalColumnsRenamed.withColumnRenamed(pivoted.columns(pivoted.columns.length - 1), "Cluster")
-        finalColumnsRenamed
+        finalDF.show()
+
+        //do the database writing:
+        writeDFToDB(finalDF,config.configIdentifier)
+        writeDFToDB(centerDF.toDF(),config.configIdentifier + "_centers")
     }
 
     def getInputDataframe = getArbitraryQueryResult(config.url,config.query)
--- src/main/scala/de/hpi/data_change/time_series_similarity/Main.scala
+++ src/main/scala/de/hpi/data_change/time_series_similarity/Main.scala
@@ -1,8 +1,6 @@
 package de.hpi.data_change.time_series_similarity
 
-import java.util.Properties
-
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 /**
   * Main entry to the clustering framework
@@ -18,63 +16,10 @@ object Main extends App with Serializable{
   if(isLocal) {
     sparkBuilder = sparkBuilder.master("local[2]")
   }
-
-  import java.sql.Connection
-  import java.sql.DriverManager
-
-  val databaseName = "db"
-  val user = "monetdb"
-  val pw = "monetdb"
-  val tableName = "wiki"
-
-  private val address = "172.16.64.69"
-  private val port = "5050"
-  val driver = "nl.cwi.monetdb.jdbc.MonetDriver"
-  val url = s"jdbc:monetdb://$address:$port/$databaseName"
-  val con = DriverManager.getConnection(url, s"$user", s"$pw")
-  val st = con.createStatement()
-  val query = s"SELECT * FROM $tableName LIMIT 100"
-  val result = st.executeQuery(query)
-  val resultTableName = "testResultTable"
-
-  //print results:
-  while(result.next()){
-    println(result.getString(2));
-  }
-
-  def getArbitraryQueryResult(url:String,query:String) = {
-    spark.sqlContext.read.format("jdbc").
-        option("url", url).
-        option("driver", driver).
-        option("useUnicode", "true").
-        //option("continueBatchOnError","true").
-        option("useSSL", "false").
-        option("user", user).
-        option("password", pw).
-        option("dbtable","(" + query + ") as queryresult").
-        load()
-  }
-
-  def writeToDB(resultDF: DataFrame) = {
-    val toWrite = resultDF
-    println("dummy output simulating database write")
-    val props = new Properties()
-    props.setProperty("user",user)
-    props.setProperty("password",pw)
-    props.setProperty("driver",driver)
-    toWrite.write.jdbc(url,resultTableName,props)
-  }
-
   val spark = sparkBuilder.getOrCreate()
-  val df = spark.read.csv("resources/test.csv")
-  df.show()
-  val df2 = getArbitraryQueryResult(url,query)
-  df2.show(1000)
-  //writeToDB(df2)
-
 //  val clusterer = new Clustering(spark)
 //  clusterer.setParams(args(0))
 //  clusterer.grouper = (cr => Seq(cr.entity))
 //  clusterer.groupFilter = (ds => ds.filter(t => t._2.size>50))
-//  clusterer.clustering()
+  clusterer.clustering()
 }
